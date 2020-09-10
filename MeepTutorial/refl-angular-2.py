7fa38500e6570c14f2a3f956539d124df4d8439b
Vall <anirix.vall@gmail.com>
2020-09-10 12:58:24 -0300

    'refl-angular': Function adaptation + Minor changes. F1+N1+S1.
    New:
    	'MeepTutorial/refl-angular-2' contains a functional approach adaptation of the original script.
    
    Functional:
    	Made a functional approach adaptation.
    
    Syntaxis:
    	Comments.

--- MeepTutorial/refl-angular-2.py
+++ MeepTutorial/refl-angular-2.py
@@ -1,23 +1,37 @@
 # -*- coding: utf-8 -*-
 
-# From Meep Tutorial: Planar interface :)
+# Modified from Meep Tutorial: Planar interface :)
 
+import numpy as np
 import meep as mp
-import argparse
 import math
+# import h5py as h5
+# import os
+import matplotlib.pyplot as plt
 
 #%%
 
-def main(args):
+def Fresnel_planar_interface(theta, resolution=200):
+    """Reflectance and transmittance on planar interface.
     
-    resolution = args.res
+    Parameters
+    ----------
+    theta : int, float
+        Angle of incidence CCW from the normal, expressed in degrees.
+    resolution=200 : int, optional
+        Cells per μm.
+    
+    Returns
+    -------
+    
+    """
 
     dpml = 1.0                      # PML thickness                
     sz = 10 + 2*dpml                # 10 is size of cell without PMLs
     # A 1d cell must be used since a higher-dimensional cell 
     # will introduce artificial modes due to band folding.
     
-    cell_size = mp.Vector3(0,0,sz)
+    cell_size = mp.Vector3(0, 0, sz)
     pml_layers = [mp.PML(dpml)]
 
     # Gaussian w/visible spectrum
@@ -32,7 +46,7 @@ def main(args):
     
     # Rotation angle of planewave
     # (input in degrees and transformed to radians)
-    theta_r = math.radians(args.theta)
+    theta_r = math.radians(theta)
     # Counterclockwise (CCW) around Y axis
     # 0 degrees along +Z axis
 
@@ -68,19 +82,19 @@ def main(args):
                         dimensions=dimensions,
                         resolution=resolution)
 
-    refl_fr = mp.FluxRegion(center=mp.Vector3(0,0,-0.25*sz))
+    refl_fr = mp.FluxRegion(center=mp.Vector3(0, 0, -0.25*sz))
     refl = sim.add_flux(fcen, df, nfreq, refl_fr)
     
     sim.run(until_after_sources=mp.stop_when_fields_decayed(
-        50, mp.Ex, mp.Vector3(0,0,-0.5*sz+dpml), 1e-9))
+        50, mp.Ex, mp.Vector3(0, 0, -0.5*sz+dpml), 1e-9))
 
     empty_flux = mp.get_fluxes(refl)
     empty_data = sim.get_flux_data(refl)
     sim.reset_meep()
 
     # add a block with n=3.5 for the air-dielectric interface
-    geometry = [mp.Block(mp.Vector3(mp.inf,mp.inf,0.5*sz), 
-                         center=mp.Vector3(0,0,0.25*sz), 
+    geometry = [mp.Block(mp.Vector3(mp.inf, mp.inf, 0.5*sz), 
+                         center=mp.Vector3(0, 0, 0.25*sz), 
                          material=mp.Medium(index=3.5))]
 
     sim = mp.Simulation(cell_size=cell_size,
@@ -95,21 +109,22 @@ def main(args):
     sim.load_minus_flux_data(refl, empty_data)
 
     sim.run(until_after_sources=mp.stop_when_fields_decayed(
-        50, mp.Ex, mp.Vector3(0,0,-0.5*sz+dpml), 1e-9))
+        50, mp.Ex, mp.Vector3(0, 0, -0.5*sz+dpml), 1e-9))
 
-    refl_flux = mp.get_fluxes(refl)
-    freqs = mp.get_flux_freqs(refl)
-
-    for i in range(nfreq):
-        print("refl:, {}, {}, {}, {}".format(
-            k.x,
-            1/freqs[i],
-            math.degrees(math.asin(k.x/freqs[i])), # if freq is freqmin
-            -refl_flux[i]/empty_flux[i]))
+    refl_flux = np.array(mp.get_fluxes(refl))
+    freqs = np.array(mp.get_flux_freqs(refl))
+    wavelengths = 1/freqs
+    angles = np.degrees( np.arcsin(k.x/freqs) ) # 90 if freq = freqmin?
+    # Angle for the (kx,ω) pair.
+    reflectances = -refl_flux / np.array(empty_flux)
+       
+    # f = h5.File("MeepTutorial/FresnelResults/FresnelResults.h5", "w")
+    # f.create_dataset("")
     
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-res', type=int, default=200, help='resolution (default: 200 pixels/um)')
-    parser.add_argument('-theta', type=float, default=0, help='angle of incident planewave (default: 0 degrees)')
-    args = parser.parse_args()
-    main(args)
+    return k.x, wavelengths, angles, reflectances
+
+#%%
+
+kx, wavelengths, angles, reflectances = Fresnel_planar_interface(0)
+
+plt.plot(wavelengths, reflectances)
