3b4a6e2c25d041b57d848701db9b70f2be129968
Vall <anirix.vall@gmail.com>
2020-10-05 13:27:38 -0300

    VClass: 2 new old classes. N2+S1.
    New:
    	'WrapperDict' is a subclass example that applies methods to all items of dict.
    	'WrapperList' is a subclass example that applies methods to all items of a list.
    
    Syntaxis:
    	Updated some documentation.

--- v_class.py
+++ v_class.py
@@ -1,15 +1,187 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-The 'vsClasses' module contains customed classes.
+The 'v_class' module contains customed classes.
 
-The only class contained right now is...
+WrapperList : class
+    A list subclass that applies methods to a list of instances.
+WrapperDict : class
+    A dict subclass that applies methods to a dict of instances.
 DottableWrapper : class
     Example of a class which allows dot calling instances.
 
-
 @author: Vall
 """
+
+#%%
+
+class WrapperList(list):
+    
+    """A list subclass that applies methods to a list of instances.
+    
+    Examples
+    --------
+    >> class MyClass:
+    
+        def __init__(self, value=10):
+            self.sub_prop = value
+            self._prop = value
+        
+        @property
+        def prop(self):
+            return self._prop
+        
+        @prop.setter
+        def prop(self, value):
+            self._prop = value
+            
+        def sub_method(self, item):
+            return item * self.sub_prop
+        
+        def method(self, item):
+            return item * self.prop
+        
+    >> Z = WrapperList([MyClass(), MyClass(2)])
+    >> Z.prop
+    [10, 2]
+    >> Z._prop
+    [10, 2]
+    >> Z.sub_prop
+    [10, 2]
+    >> Z.method(2)
+    [20, 4]
+    >> Z.prop = 3
+    >> Z.prop
+    [3, 3]
+    >> Z.append(MyClass(1))
+    >> Z.prop
+    [3, 3, 1]
+    >> Z.prop = [10, 2, 1]
+    >> Z.prop
+    [10, 2, 1]
+    
+    Warnings
+    --------
+    >> Z.prop = [2, 3]
+    >> Z.prop
+    [[2,3], [2,3], [2,3]]
+    
+    """
+
+    def __init__(self, iterable=[]):
+        
+        super().__init__(iterable)
+
+    def __transf_methods__(self, methods_list):
+        
+        def function(*args, **kwargs):
+            results = [m(*args, **kwargs) for m in methods_list]
+            return results
+        
+        return function
+
+    def __getattr__(self, name):
+        
+        if name in dir(self):
+        
+            super().__getattribute__(name)
+        
+        else:
+            
+            result = []
+            for ins in self:
+                result.append(ins.__getattribute__(name))
+            if callable(result[0]):
+                result = self.__transf_methods__(result)
+            return result
+    
+    def __setattr__(self, name, value):
+        
+        if isinstance(value, list) and len(value) == len(self):
+            for ins, v in zip(self, value):
+                ins.__setattr__(name, v)
+        else:
+            for ins in self:
+                ins.__setattr__(name, value)
+
+class WrapperDict(dict):
+    
+    """A dict subclass that applies methods to a dict of instances.
+    
+    Examples
+    --------
+    >> class MyClass:
+    
+        def __init__(self, value=10):
+            self.sub_prop = value
+            self._prop = value
+        
+        @property
+        def prop(self):
+            return self._prop
+        
+        @prop.setter
+        def prop(self, value):
+            self._prop = value
+            
+        def sub_method(self, item):
+            return item * self.sub_prop
+        
+        def method(self, item):
+            return item * self.prop
+        
+    >> Z = WrapperDict(a=MyClass(), b=MyClass(2))
+    >> Z.prop
+    {'a': 10, 'b': 2}
+    >> Z.update(c=MyClass(1))
+    >> Z.prop
+    {'a': 10, 'b': 2, 'c': 1}
+    >> Z.method(2)
+    {'a': 20, 'b': 4, 'c': 2}
+    >> Z.prop = 3
+    >> Z.prop
+    {'a': 3, 'b': 3, 'c': 3}
+    >> Z.prop = {'a': 10, 'b': 2, 'c': 1}
+    >> Z.prop
+    {'a': 10, 'b': 2, 'c': 1}
+    
+    """
+
+    def __init__(self, **elements):
+        
+        super().__init__(**elements)
+    
+    def __transf_methods__(self, methods_dic):
+        
+        def function(*args, **kwargs):
+            results = {}
+            for key, method in methods_dic.items():
+                results.update({key: method(*args, **kwargs)})
+            return results
+        
+        return function
+    
+    def __getattr__(self, name):
+        
+        if name in dir(self):
+            super().__getattribute__(name)        
+        else:
+            result = {n : ins.__getattribute__(name) 
+                      for n, ins in self.items()}
+            if callable(list(result.values())[0]):
+                result = self.__transf_methods__(result)
+            return result
+    
+    def __setattr__(self, name, value):
+        
+        if isinstance(value, dict):
+            for ins, v in zip(self.values(), value.values()):
+                ins.__setattr__(name, v)
+        else:
+            for ins in self.values():
+                ins.__setattr__(name, value)
+
+#%%
 
 class DottableWrapper:
     
@@ -79,6 +251,14 @@ class DottableWrapper:
         
         self.all = WrapperDict()
         self.add(**instances)
+
+    # def __print__(self):
+        
+    #     self.all.__print__()
+        
+    # def __repr__(self):
+        
+    #     self.all.__repr__()
     
     def add(self, **instances):
         
