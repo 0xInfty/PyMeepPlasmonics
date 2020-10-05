68bd803500338980c5cbbba2911820505faf4c83
Vall <anirix.vall@gmail.com>
2020-10-05 20:40:16 -0300

    'mie_scattering': Our script's Mie with several changes. F5+S1+D1+W1.
    Functional:
    	changed length unit from 1 nm to 10 nm.
    	changed resolution to 2 pixels per length unit.
    	changed air width to be independant of wavelength.
    	redefined "until_after_sources" as enough time for the pulse to travel across the entire cell.
    	defined refraction index as sqrt of epsilon*mu to be able to compute the theory: correctly! <3 Thanks, Lu.
    
    Syntaxis:
    	added comments with computation enlapsed time intervals.
    
    Debug:
    	corrected medium in geometry definition.
    
    Warnings:
    	some frequencies are too small for the medium, should maybe change that next time to 350-850 nm instead of 200-800 nm.

--- mie_scattering.py
+++ mie_scattering.py
@@ -7,6 +7,7 @@
 import meep as mp
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 import PyMieScatt as ps
 from v_materials import import_medium
 from v_units import MeepUnitsManager
@@ -15,16 +16,16 @@ from v_units import MeepUnitsManager
 
 ### MEAN PARAMETERS
 
-# Units: 1 nm as length unit
-from_um_factor = 1e-3 # Conversion of 1 μm to my length unit (=1nm/1μm)
-resolution = 25 # >=8 pixels per smallest wavelength, i.e. np.floor(8/wvl_min)
+# Units: 10 nm as length unit
+from_um_factor = 10e-3 # Conversion of 1 μm to my length unit (=10nm/1μm)
+resolution = 2 # >=8 pixels per smallest wavelength, i.e. np.floor(8/wvl_min)
 
 # Dielectric sphere
-r = 60  # Radius of sphere: 60 nm
+r = 6  # Radius of sphere: 60 nm
 medium = import_medium("Au", from_um_factor) # Medium of sphere: gold (Au)
 
 # Frequency and wavelength
-wlen_range = np.array([200,800]) # nm range
+wlen_range = np.array([20,80]) # 200-800 nm range from lowest to highest
 nfreq = 100 # Number of frequencies to discretize range
 
 ### OTHER PARAMETERS
@@ -33,13 +34,13 @@ nfreq = 100 # Number of frequencies to discretize range
 uman = MeepUnitsManager(from_um_factor=from_um_factor)
 
 # Frequency and wavelength
-freq_range = 1/wlen_range # Hz range in Meep units
+freq_range = 1/wlen_range # Hz range in Meep units from highest to lowest
 freq_center = np.mean(freq_range)
 freq_width = max(freq_range) - min(freq_range)
 
 # Space configuration
 pml_width = 0.5 * max(wlen_range)
-air_width = 0.5 * min(wlen_range)
+air_width = 2 * r # 0.5 * max(wlen_range)
 
 #%% FIRST RUN: GEOMETRY SETUP
 
@@ -96,7 +97,44 @@ box_z2 = sim.add_flux(freq_center, freq_width, nfreq,
 
 #%% FIRST RUN: SIMULATION NEEDED TO NORMALIZE
 
-sim.run(until_after_sources=10)
+temp = time()
+sim.init_sim()
+enlapsed = time() - temp
+
+"""
+112x112x112 with resolution 4
+(48 cells inside diameter)
+==> 30 s to build
+
+112x112x112 with resolution 2
+(24 cells inside diameter)
+==> 4.26 s to build
+
+116x116x116 with resolution 2
+(24 cells inside diameter)
+==> 5.63 s
+
+172x172x172 with resolution 2
+(24 cells inside diameter)
+==> 17.47 s
+"""
+
+temp = time()
+sim.run(until_after_sources=1.1*cell_width) 
+# Enough time for the pulse to pass through all the cell
+enlapsed = [enlapsed, time()-temp]
+# Originally: Aprox 3 periods of lowest frequency, using T=λ/c=λ in Meep units 
+# Now: Aprox 3 periods of highest frequency, using T=λ/c=λ in Meep units 
+
+"""
+112x112x112 with resolution 2
+(24 cells inside diameter)
+==> 135.95 s to complete 1st run
+
+116x116x116 with resolution 2
+(24 cells inside diameter)
+==> 208 s to complete 1st run
+"""
 
 freqs = mp.get_flux_freqs(box_x1)
 box_x1_data = sim.get_flux_data(box_x1)
@@ -112,7 +150,7 @@ sim.reset_meep()
 
 #%% SECOND RUN: GEOMETRY SETUP
 
-geometry = [mp.Sphere(material=mp.Medium(index=n_sphere),
+geometry = [mp.Sphere(material=medium,
                       center=mp.Vector3(),
                       radius=r)]
 # Lossless dielectric sphere 
@@ -145,16 +183,45 @@ box_z2 = sim.add_flux(freq_center, freq_width, nfreq,
                       mp.FluxRegion(center=mp.Vector3(z=+r),
                                     size=mp.Vector3(2*r,2*r,0)))
 
+temp = time()
+sim.init_sim()
+enlapsed.append( time() - temp )
+
+"""
+112x112x112 with resolution 2
+(24 cells in diameter)
+==> 5.16 s to build with sphere
+
+116x116x116 with resolution 2
+(24 cells inside diameter)
+==> 9.71 s to build with sphere
+"""
+
+temp = time()
 sim.load_minus_flux_data(box_x1, box_x1_data)
 sim.load_minus_flux_data(box_x2, box_x2_data)
 sim.load_minus_flux_data(box_y1, box_y1_data)
 sim.load_minus_flux_data(box_y2, box_y2_data)
 sim.load_minus_flux_data(box_z1, box_z1_data)
 sim.load_minus_flux_data(box_z2, box_z2_data)
+enlapsed.append( time() - temp )
+
+"""
+112x112x112 with resolution 2
+(24 cells in diameter)
+==> 0.016 s to add flux
+
+116x116x116 with resolution 2
+(24 cells inside diameter)
+==> 0.021 s to add flux
+"""
 
 #%% SECOND RUN: SIMULATION :D
 
-sim.run(until_after_sources=100)
+temp = time()
+sim.run(until_after_sources=10*1.1*cell_width) 
+enlapsed.append( time() - temp )
+# Aprox 30 periods of lowest frequency, using T=λ/c=λ in Meep units 
 
 box_x1_flux = mp.get_fluxes(box_x1)
 box_x2_flux = mp.get_fluxes(box_x2)
@@ -183,21 +250,26 @@ scatt_eff_meep = -1 * scatt_cross_section / (np.pi*r**2)
 # Scattering efficiency =
 # = scattering cross section / cross sectional area of the sphere
 
-scatt_eff_theory = [ps.MieQ(n_sphere, 
-                            1000/f,
-                            2*r*1000,
+freqs = np.array(freqs)
+freqs_trim = freqs[freqs<0.0403274589668105]
+
+scatt_eff_theory = [ps.MieQ(np.sqrt(medium.epsilon(f)[0,0]*medium.mu(f)[0,0]), 
+                            10/f,
+                            2*r*10,
                             asDict=True)['Qsca'] 
-                    for f in freqs]
+                    for f in freqs_trim]
 # The simulation results are validated by comparing with 
 # analytic theory of PyMieScatt module
 
 plt.figure(dpi=150)
-plt.loglog(2*np.pi*r*np.asarray(freqs),
-           scatt_eff_meep,'bo-',label='Meep')
-plt.loglog(2*np.pi*r*np.asarray(freqs),
-           scatt_eff_theory,'ro-',label='Theory')
-plt.grid(True,which="both",ls="-")
-plt.xlabel('Circumference/Wavelength [2πr/λ]')
+plt.plot(10/freqs, scatt_eff_meep,'bo-',label='Meep')
+plt.plot(10/freqs_trim, scatt_eff_theory,'bo-',label='Theory')
+# plt.loglog(2*np.pi*r*np.asarray(freqs),
+#            scatt_eff_meep,'ro-',label='Meep')
+# plt.loglog(2*np.pi*r*np.asarray(freqs),
+#            scatt_eff_theory,'ro-',label='Theory')
+# plt.grid(True,which="both",ls="-")
+plt.xlabel('Wavelength [nm]')
 plt.ylabel('Scattering efficiency [σ/πr$^{2}$]')
 plt.legend()
 plt.title('Mie Scattering of a Lossless Dielectric Sphere')
