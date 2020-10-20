1d0e107c36883811aacfa8fca7250b529e44b68e
Vall <anirix.vall@gmail.com>
2020-10-20 14:02:03 -0300

    'au...compared': Working on another gif. F1+S1+D1.
    Functional:
    	now working on the X=Y=0 gif.
    
    Syntaxys:
    	updated comments.
    
    Debug:
    	corrected label in gif.

--- au_sphere_compared.py
+++ au_sphere_compared.py
@@ -45,10 +45,13 @@ results_line = [fi["Ez"] for fi in f]
 results_plane = [gi["Ez"] for gi in g]
 
 resolution = [rp.attrs["resolution"] for rp in results_plane]
+r = [rp.attrs["r"] for rp in results_plane]
 cell_width = [rp.attrs["cell_width"] for rp in results_plane]
 pml_width = [rp.attrs["pml_width"] for rp in results_plane]
 period_plane = [rp.attrs["period_plane"] for rp in results_plane]
 wlen = [rp.attrs["wlen"] for rp in results_plane]
+air_width = [cell_width[j]/2 - pml_width[j] - r[j] for j in range(n)]
+
 
 index_to_space = lambda i, j : i/resolution[j] - cell_width[j]/2
 space_to_index = lambda x, j : round(resolution[j] * (x + cell_width[j]/2))
@@ -81,7 +84,7 @@ for sfs in source_field_in_fase:
 
 plt.savefig(file("Source.png"))
 
-#%% NEW PLANE IN FASE GIF
+#%% PLANE IN FASE GIF
 
 results_plane_in_fase = [rp[i:f,:,:] for rp, i, f in zip(results_plane, 
                                                          init_index, 
@@ -94,7 +97,7 @@ nframes = max(all_nframes)
 jmax = np.where(np.asarray(all_nframes)==nframes)[0][0]
 call_index = lambda i, j : int(i * all_nframes[j] / nframes)
 call_series = lambda i, j : results_plane_in_fase[j][i,:,:].T
-label_function = lambda i : 'Tiempo: {:.1f}'.format(i * period_plane[jmax] / wlen[jmax]) + ' $T_{max}$'
+label_function = lambda i : 'Tiempo: {:.1f}'.format(i * period_plane[jmax] / wlen[jmax]) + ' T'
 
 # Animation base
 fig = plt.figure(figsize=(n*6.4, 6.4))
@@ -147,3 +150,48 @@ def make_gif_plane(gif_filename):
 make_gif_plane(file("PlaneX=0"))
 plt.close(fig)
 # del fig, ax, lims, nframes_step, nframes, call_series, label_function
+
+#%% MAKE Z LINES GIF
+
+z_profile = [results_plane[j][:, space_to_index(0,j), :] for j in range(n)]
+
+# What should be parameters
+nframes_step = 1
+all_nframes = [int(rp.shape[0]/nframes_step) for rp in results_plane_in_fase]
+nframes = max(all_nframes)
+jmax = np.where(np.asarray(all_nframes)==nframes)[0][0]
+call_index = lambda i, j : int(i * all_nframes[j] / nframes)
+call_series = lambda i, j : z_profile[j][i,:]
+label_function = lambda i : 'Tiempo: {:.1f}'.format(i * period_plane[jmax] / wlen[jmax]) + ' T'
+
+# Animation base
+fig = plt.figure()
+ax = plt.subplot()
+lims = lambda j : (np.min(z_profile[j]), np.max(z_profile[j]))
+shape = [call_series(0,i).shape[0] for i in range(n)]
+
+def make_pic_line(i):
+    ax.clear()
+    plt.plot(np.linspace(-cell_width/2, cell_width/2, shape), 
+             call_series(i))
+    ax.set_ylim(*lims)
+    ax.text(-.12, -.1, label_function(i), transform=ax.transAxes)
+    plt.xlabel("Distancia en z (u.a.)")
+    plt.ylabel("Campo eléctrico Ez (u.a.)")
+    plt.show()
+    return ax
+
+def make_gif_line(gif_filename):
+    pics = []
+    for i in range(nframes):
+        ax = make_pic_line(i*nframes_step)
+        plt.savefig('temp_pic.png') 
+        pics.append(mim.imread('temp_pic.png')) 
+        print(str(i+1)+'/'+str(nframes))
+    mim.mimsave(gif_filename+'.gif', pics, fps=5)
+    os.remove('temp_pic.png')
+    print('Saved gif')
+
+make_gif_line(file("AxisZ"))
+plt.close(fig)
+# del fig, ax, lims, nframes_step, nframes, call_series, label_functio
