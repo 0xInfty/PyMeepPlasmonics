2a585426bfaaf5329e71016ad6031a70982c032e
Vall <anirix.vall@gmail.com>
2021-03-10 19:54:25 -0300

    'DataAn...submerged': Max wavelength, adaptation and expansion. F3.
    Functional:
    	Now using more 'vs' functions as a result of adaptation to changes in the module.
    	Now using color map for lines like other scripts.
    	Now calculates wavelength for maximum scattering.

--- DataAnalysis/au_mie_sphere_submerged.py
+++ DataAnalysis/au_mie_sphere_submerged.py
@@ -8,18 +8,19 @@ Created on Wed Nov 11 12:24:04 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pylab as plab
 import os
 import v_save as vs
 
 #%% PARAMETERS
 
 # Saving directories
-vacuum_folder = "AuMieSphere/AuMie/7)Diameters/WLen5065"
+vacuum_folder = "AuMieSphere/AuMie/7)Diameters/WLen4560"
 water_folder = "AuMieMediums/AllWater"
+glassnwater_folder = "AuMieMediums/GlassNWater"
 marian_folder = "AuMieSphere/AuMie/7)Diameters/Marians"
-home = vs.get_home()
 
-# nprocesses = np.arange(8) + 1
+home = vs.get_home()
 
 #%% LOAD VACUUM DATA
 
@@ -27,8 +28,8 @@ vacuum_path = os.path.join(home, vacuum_folder)
 vacuum_file = lambda f, s : os.path.join(vacuum_path, f, s)
 
 vacuum_series = os.listdir(vacuum_path)
-vacuum_series.sort()
-vacuum_series = [*vacuum_series[1:4], vacuum_series[0]]#, *series[4:]] # For AllWater
+vacuum_series = vs.filter_by_string_must(vacuum_series, "SC")
+vacuum_series = vs.sort_by_number(vacuum_series, 0)
 
 vacuum_data = []
 vacuum_params = []
@@ -37,14 +38,7 @@ for s in vacuum_series:
     vacuum_params.append(vs.retrieve_footer(vacuum_file(s, "Results.txt")))
 vacuum_header = vs.retrieve_header(vacuum_file(s, "Results.txt"))
 
-fixed_params = []
-for p in vacuum_params:
-    problem = p.split("wlen_range=")[1].split(", nfreq")[0]
-    solved = ", ".join(problem.split(" "))
-    fixed = solved.join(p.split(problem))
-    fixed_params.append(eval(f"dict({fixed})"))
-params = fixed_params
-del p, problem, solved, fixed, fixed_params
+vacuum_params = [vs.fix_params_dict(p) for p in vacuum_params]
 
 #%% LOAD WATER DATA
 
@@ -52,8 +46,8 @@ water_path = os.path.join(home, water_folder)
 water_file = lambda f, s : os.path.join(water_path, f, s)
 
 water_series = os.listdir(water_path)
-water_series.sort()
-water_series = [*water_series[1:4], water_series[0]]#, *series[4:]] # For AllWater
+water_series = vs.filter_by_string_must(water_series, "AllWater")
+water_series = vs.sort_by_number(water_series, 0)
 
 water_data = []
 water_params = []
@@ -62,14 +56,24 @@ for s in water_series:
     water_params.append(vs.retrieve_footer(water_file(s, "Results.txt")))
 water_header = vs.retrieve_header(water_file(s, "Results.txt"))
 
-fixed_params = []
-for p in water_params:
-    problem = p.split("wlen_range=")[1].split(", nfreq")[0]
-    solved = ", ".join(problem.split(" "))
-    fixed = solved.join(p.split(problem))
-    fixed_params.append(eval(f"dict({fixed})"))
-params = fixed_params
-del p, problem, solved, fixed, fixed_params
+water_params = [vs.fix_params_dict(p) for p in water_params]
+
+#%% LOAD GLASS N WATER DATA
+
+glassnwater_path = os.path.join(home, glassnwater_folder)
+glassnwater_file = lambda f, s : os.path.join(glassnwater_path, f, s)
+
+glassnwater_series = os.listdir(glassnwater_path)
+glassnwater_series = vs.sort_by_number(glassnwater_series, 0)
+
+glassnwater_data = []
+glassnwater_params = []
+for s in glassnwater_series:
+    glassnwater_data.append(np.loadtxt(glassnwater_file(s, "Results.txt")))
+    glassnwater_params.append(vs.retrieve_footer(glassnwater_file(s, "Results.txt")))
+glassnwater_header = vs.retrieve_header(glassnwater_file(s, "Results.txt"))
+
+glassnwater_params = [vs.fix_params_dict(p) for p in glassnwater_params]
 
 #%% LOAD MARIAN'S DATA
 
@@ -77,11 +81,13 @@ marian_path = os.path.join(home, marian_folder)
 marian_file = lambda s : os.path.join(marian_path, s)
 
 marian_series = os.listdir(marian_path)
-marian_series.sort()
-marian_exp_series = marian_series[1:5]#marian_series[-4:]
-marian_exp_series = [*marian_exp_series[1:], marian_exp_series[0]]
-marian_mie_series = marian_series[-4:]
-marian_mie_series = [*marian_mie_series[1:], marian_mie_series[0]]
+
+marian_exp_series = vs.filter_by_string_must(marian_series, "exp")
+marian_exp_series = vs.filter_by_string_must(marian_exp_series, "glass")
+marian_exp_series = vs.sort_by_number(marian_exp_series)
+
+marian_mie_series = vs.filter_by_string_must(marian_series, "mie")
+marian_mie_series = vs.sort_by_number(marian_mie_series)
 
 marian_exp_data = []
 for s in marian_exp_series:
@@ -91,6 +97,15 @@ marian_mie_data = []
 for s in marian_mie_series:
     marian_mie_data.append(np.loadtxt(marian_file(s)))
 
+#%% GET MAX WAVELENGTH
+
+vacuum_max = [vacuum_data[i][np.argmax(vacuum_data[i][:,1]), 0] for i in range(len(vacuum_data))]
+water_max = [water_data[i][np.argmax(water_data[i][:,1]), 0] for i in range(len(water_data))]
+glassnwater_max = [glassnwater_data[i][np.argmax(glassnwater_data[i][:,1]), 0] for i in range(len(glassnwater_data))]
+
+marian_exp_max = [marian_exp_data[i][np.argmax(marian_exp_data[i][:,1]), 0] for i in range(len(marian_exp_data))]
+marian_mie_max = [marian_mie_data[i][np.argmax(marian_mie_data[i][:,1]), 0] for i in range(len(marian_mie_data))]
+
 #%% PLOT
 
 diameters = [48, 64, 80, 103]
@@ -98,23 +113,48 @@ colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']
 
 plt.figure()
 for vdt, d, c in zip(vacuum_data, diameters, colors):
-    scatt = vdt[:,1]
-    scatt = ( scatt - min(scatt) ) / (max(scatt) - min(scatt))
-    plt.plot(vdt[:,0], scatt, linestyle='solid', color=c, label=f"Meep Vacío {d} nm") #d[:,1])
-    # plt.plot(dt[:,0], dt[:,1], label=f"Meep {d} nm")
+    plt.plot(vdt[:,0], vdt[:,1] / max(vdt[:,1]), #* np.pi * (d**2) / 4
+             linestyle='solid', color=c, label=f"Meep Vacío {d} nm")
 for wdt, d, c in zip(water_data, diameters, colors):
-    scatt = wdt[:,1]
-    scatt = ( scatt - min(scatt) ) / (max(scatt) - min(scatt))
-    plt.plot( wdt[:,0], scatt, linestyle='dashdot', color=c, label=f"Meep Agua {d} nm") #d[:,1])
-    # plt.plot(dt[:,0], dt[:,1], label=f"Meep {d} nm")
+    plt.plot( wdt[:,0], wdt[:,1] / max(wdt[:,1]), 
+             linestyle='dashdot', color=c, label=f"Meep Agua {d} nm")
 plt.style.use('default')
 for mdt, d, c in zip(marian_exp_data, diameters, colors):
-    plt.plot(mdt[:,0], mdt[:,1], linestyle='dashed', color=c, label=f"Exp {d} nm")
+    plt.plot(mdt[:,0], mdt[:,1] / max(mdt[:,1]), 
+             linestyle='dashed', color=c, label=f"Marian Exp {d} nm")
 plt.style.use('default')
 for mdt, d, c in zip(marian_mie_data, diameters, colors):
-    plt.plot(mdt[:,0], mdt[:,1], linestyle="dotted", color=c, label=f"Mie {d} nm")
+    plt.plot(mdt[:,0], mdt[:,1] / max(mdt[:,1]), 
+             linestyle="dotted", color=c, label=f"Marian Mie {d} nm")
+plt.xlabel("Wavelength [nm]")
+plt.ylabel("Normalized Scattering [a.u.]")
+plt.xlim([450, 650])
+plt.legend(ncol=4, framealpha=1)
+vs.saveplot(water_file("", "AllScatt.png"), overwrite=True)
+
+#%% PLOT 103 nm
+
+colors = (c for c in plab.cm.Reds(np.linspace(0,1,2+5))[2:])
+
+plt.figure()
+plt.title("Scattering of Au sphere (103 nm diameter)")
+scatt = vacuum_data[-1][:,1] #* np.pi * (diameters[-1]**2) / 4
+plt.plot(vacuum_data[-1][:,0], scatt/max(scatt),
+         linestyle="solid", color=next(colors), label="Vacuum")
+scatt = water_data[-1][:,1] #* np.pi * (diameters[-1]**2) / 4
+plt.plot(water_data[-1][:,0], scatt/max(scatt),
+         linestyle="dashdot", color=next(colors), label="Water")
+scatt = glassnwater_data[-1][:,1] #* np.pi * (diameters[-1]**2) / 4
+plt.plot(glassnwater_data[0][:,0], scatt/max(scatt),
+         linestyle="dashed", color=next(colors), label="Glass+Water")
+plt.plot(marian_exp_data[-1][:,0], 
+         marian_exp_data[-1][:,1] / max(marian_exp_data[-1][:,1]), 
+         linestyle="dotted", color=next(colors), label="Experimental")
+plt.plot(marian_mie_data[-1][:,0], 
+         marian_mie_data[-1][:,1] / max(marian_mie_data[-1][:,1]), 
+         linestyle="dotted", color=next(colors), label="Mie Theory Water")
 plt.xlabel("Wavelength [nm]")
 plt.ylabel("Rescaled Scattering [a.u.]")
-plt.xlim([500, 650])
-plt.legend(loc="lower center", ncol=4, framealpha=1)
-vs.saveplot(water_file("", "AllScatt.png"))
+plt.xlim([450, 650])
+plt.legend(framealpha=1)
+vs.saveplot(water_file("", "Scatt103.png"), overwrite=True)
