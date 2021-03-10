8be70a05b8b6050de69ef568e6f697a8a15dac39
Vall <anirix.vall@gmail.com>
2021-03-10 13:10:14 -0300

    'DataA...generic...': Now allows several series' folders. F1.
    Functional:
    	Now it allows for several folders to be specified as supra series with different colors or linestyles.

--- DataAnalysis/scattering_generic_comparison.py
+++ DataAnalysis/scattering_generic_comparison.py
@@ -15,13 +15,17 @@ import v_save as vs
 #%% PARAMETERS
 
 # Saving directories
-folder = "AuMieMediums/AllWaterTest"
+folder = ["AuMieMediums/AllWaterTest"]
 home = vs.get_home()
 
 # Sorting and labelling data series
-sorting_method = "classic"#"number2"
-series_label = lambda s : "" #lambda s : f"Second Time Factor x{vs.find_numbers(s)[2]}"
-recognize_string = "BackToVacuum" # leave "" per default
+sorting_method = ["classic"] # "number2"
+series_label = [lambda s : s] #lambda s : f"Second Time Factor x{vs.find_numbers(s)[2]}"
+series_must = ["BackToVacuum"] # leave "" per default
+
+# Scattering plot options
+series_colors = [plab.cm.Blues]
+series_linestyles = ["solid"]
 
 #%% SORTING METHOD
 
@@ -44,53 +48,70 @@ def sorting_function_definer(sorting_method):
     
     return sorting_function
 
-sorting_function = sorting_function_definer(sorting_method)
+sorting_function = [sorting_function_definer(sm) for sm in sorting_method]
 
 # sorting_function = lambda l : [*l[-4:-1], l[-5]]
 
 #%% LOAD DATA
 
-path = os.path.join(home, folder)
-file = lambda f, s : os.path.join(path, f, s)
-
-series = os.listdir(path)
-to_remove_items = []
-for s in series: 
-    if recognize_string not in s: to_remove_items.append(s)
-for s in to_remove_items: series.remove(s)
-del to_remove_items
-series = sorting_function(series)
-
+path = []
+file = []
+series = []
 data = []
 params = []
-for s in series:
-    data.append(np.loadtxt(file(s, "Results.txt")))
-    params.append(vs.retrieve_footer(file(s, "Results.txt")))
-header  = vs.retrieve_header(file(s, "Results.txt"))
+header = []
 
-fixed_params = []
-for p in params:
-    problem = p.split("wlen_range=")[1].split(", nfreq")[0]
-    solved = ", ".join(problem.split(" "))
-    fixed = solved.join(p.split(problem))
-    fixed_params.append(eval(f"dict({fixed})"))
-params = fixed_params
-del p, problem, solved, fixed, fixed_params
+for f, sf, sm in zip(folder, sorting_function, series_must):
 
-r = [p["r"] for p in params]
-from_um_factor = [p["from_um_factor"] for p in params]
+    path.append( os.path.join(home, f) )
+    file.append( lambda f, s : os.path.join(path[-1], f, s) )
+    
+    series.append( os.listdir(path[-1]) )
+    to_remove_items = []
+    for s in series[-1]: 
+        if sm not in s: to_remove_items.append(s)
+    for s in to_remove_items: series[-1].remove(s)
+    del to_remove_items
+    series[-1] = sf(series[-1])
+    
+    data.append( [] )
+    params.append( [] )
+    for s in series[-1]:
+        data[-1].append(np.loadtxt(file[-1](s, "Results.txt")))
+        params[-1].append(vs.retrieve_footer(file[-1](s, "Results.txt")))
+    header.append( vs.retrieve_header(file[-1](s, "Results.txt")) )
+    
+    fixed_params = []
+    for p in params[-1]:
+        problem = p.split("wlen_range=")[1].split(", nfreq")[0]
+        solved = ", ".join(problem.split(" "))
+        fixed = solved.join(p.split(problem))
+        fixed_params.append(eval(f"dict({fixed})"))
+    params[-1] = fixed_params
+    del p, problem, solved, fixed, fixed_params
+    
+    # r = [p["r"] for p in params]
+    # from_um_factor = [p["from_um_factor"] for p in params]
 
 #%% GET MAX WAVELENGTH
 
-max_wlen = [data[i][np.argmax(data[i][:,1]), 0] for i in range(len(data))]
+max_wlen = []
+for d in data:
+    max_wlen.append( [d[i][np.argmax(d[i][:,1]), 0] for i in range(len(d))] )
 
 #%% PLOT
 
-colors = plab.cm.Blues(np.linspace(0,1,len(series)+3))[3:]
+colors = [sc(np.linspace(0,1,len(s)+3))[3:] 
+          for sc, s in zip(series_colors, series)]
 
 plt.figure()
-for s, d, c, p in zip(series, data, colors, params):
-    plt.plot(d[:,0], d[:,1] / max(d[:,1]), linestyle='solid', color=c, label=series_label(s))
+for s, d, p, sl, c, ls in zip(series, data, params, series_label, 
+                              colors, series_linestyles):
+
+    for ss, sd, sp, sc in zip(s, d, p, c):
+        plt.plot(sd[:,0], sd[:,1] / max(sd[:,1]), 
+                 linestyle=ls, color=sc, label=sl(ss))
+
 plt.xlabel("Wavelength [nm]")
 plt.ylabel("Normalized Scattering Cross Section")
 plt.legend()
