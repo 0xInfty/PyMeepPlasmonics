8253e930eb18e9435c721e17ceca1f5c828a24a6
Vall <anirix.vall@gmail.com>
2021-03-15 13:25:30 -0300

    '...medmaxres...': New bash script for MaxRes submerged in a medium. N1.
    New:
    	'AuMieMediums/au_mie_medmaxres_scattering.sh' runs simulations for a MaxRes comparison submerged in a medium.

---
+++ AuMieMediums/au_mie_medmaxres_scattering.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res1" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 1 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res2" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 2 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res3" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 3 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res4" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 4 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res5" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 5 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res6" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 6 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res7" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 7 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res8" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 8 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res9" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 9 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res10" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 10 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+#mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res11" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 11 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+#mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res12" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 12 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+#mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res13" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 13 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
+#mpirun --use-hwthread-cpus -np 6 python p_au_mie_submerged_scattering.py  -s "AllWaterMax103Res14" -f "AuMieMediums/AllWaterMaxRes/AllWaterMax103Res" --from-um-factor 20e-3 -res 14 -r 2.575 --wlen-range "[25, 32.5]" -i 1.33
