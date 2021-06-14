0b2ce5516083cdbc363f3371060a8ace830feec7
Vall <anirix.vall@gmail.com>
2021-06-14 12:45:19 -0300

    `MeepTutorial/3d...`: Now profiles instead of `mayavi`. F1+D1.
    Functional:
    	added profile plotting using `matplotlib`.
    
    Debug:
    	`mayavi` doesn't work on my computer anymore, so I won't be using it.

--- MeepTutorial/3d-structure.py
+++ MeepTutorial/3d-structure.py
@@ -6,8 +6,6 @@ import meep as mp
 import numpy as np
 import math
 
-#%%
-
 cell_size = mp.Vector3(2,2,2)
 
 # A hexagon is defined as a prism with six vertices centered on the origin
@@ -25,10 +23,65 @@ sim = mp.Simulation(resolution=50,
                     cell_size=cell_size,
                     geometry=geometry)
 
+#%%
+
 sim.init_sim()
 
+x, y, z, *more = sim.get_array_metadata() # (x,y,z,w) = sim.get_array_metadata()
+# Returns coordinates and interpolation weights of the fields :)
+del more
+
 eps_data = sim.get_epsilon()
+
+#%%
 
 from mayavi import mlab
 s = mlab.contour3d(eps_data, colormap="YlGnBu")
 mlab.show()
+
+#%%
+
+import matplotlib.pyplot as plt
+# import matplotlib.pylab as plab
+
+n_slices = 3
+
+x0 = x[int(len(x)/2)]
+y0 = y[int(len(y)/2)]
+z0 = z[int(len(z)/2)]
+
+i_x0 = int(eps_data.shape[0]/2)
+i_y0 = int(eps_data.shape[1]/2)
+i_z0 = int(eps_data.shape[2]/2)
+
+titles = ["X=X0", "Y=Y0", "Z=Z0"]
+eps_slices = [eps_data[i_x0, :, :], eps_data[:, i_y0, :], eps_data[:, :, i_z0]]
+
+x_plot = [y, x, x]
+x_labels = ["Y", "X", "X"]
+y_plot = [z, z, y]
+y_labels = ["Z", "Z", "Y"]
+
+cmlims = (np.min(eps_slices), np.max(eps_slices))
+cmlims = (cmlims[0]-.1*(cmlims[1]-cmlims[0]),
+         cmlims[1]+.1*(cmlims[1]-cmlims[0]))
+
+nplots = n_slices
+fig, axes = plt.subplots(1, n_slices)
+fig.subplots_adjust(hspace=0.5, wspace=.5)
+fig.set_figheight = 6.4
+fig.set_figwidth = n_slices * 6.4
+plt.show()
+
+for ax, tl in zip(axes, titles):
+    ax.yaxis.tick_right()
+    ax.yaxis.set_label_position("right")
+    ax.set_ylabel(tl)
+
+for ax, xp, yp, eps, tl, xl, yl in zip(axes, x_plot, y_plot, eps_slices, 
+                                       titles, x_labels, y_labels):
+    ax.imshow(eps.T, interpolation='spline36', cmap='RdBu', 
+              vmin=cmlims[0], vmax=cmlims[1])
+    ax.axis("off")
+    # ax.xaxis.set_label_text(xl)
+    # ax.yaxis.set_label_text(yl)
