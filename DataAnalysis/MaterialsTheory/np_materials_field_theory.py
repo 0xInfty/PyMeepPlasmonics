8857009c17d383ee411ad969b3b8f67943425b02
Vall <anirix.vall@gmail.com>
2021-10-02 00:01:09 -0300

    `DataAnalysis/Materials`: Field & materials theory moved + Silver support + Plots. N2+F3.
    New:
    	moved two files from `DataAnalysis/Scattering/Materials` to simply `DataAnalysis/MaterialsTheory`.
    	also renamed `au_mie_field_theory` to `np_materials_field_theory` and moved it.
    
    Functional:
    	improved plots for field theory.
    	automatic naming for files and plots improved.
    	support for silver too.

--- DataAnalysis/MaterialsTheory/np_materials_field_theory.py
+++ DataAnalysis/MaterialsTheory/np_materials_field_theory.py
@@ -10,48 +10,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import v_theory as vt
+import v_plot as vp
 import v_save as vs
 import v_materials as vmt
+import v_utilities as vu
+
+english = False
+trs = vu.BilingualManager(english=english)
+vp.set_style()
 
 syshome = vs.get_sys_home()
 
 #%% PARAMETERS
 
 r = 30 # nm
-wlen_range = np.array([400, 800]) # nm
+material = "Ag"
+wlen_range = np.array([300, 800])
 wlen_chosen = [405, 532, 642]
-epsilon_ext = (1.33)**2 # Water
+submerged_index = 1.33 # Water
+
 # E0_func = lambda n : np.array([[0,0,1] for i in range(n)])
 E0 = np.array([0,0,1])
 
 npoints = 500
 
-folder = "AllWaterField2"
+folder = "DataAnalysis/MaterialsPaper"
+
+#%% MORE PARAMETERS
+
+def get_wlen_range(material, surrounding_index):
+    if material=="Au":
+        if surrounding_index == 1: return (450, 600) # (450, 650)
+        elif surrounding_index == 1.33: return (500, 650) # (500, 650)
+    else:
+        raise ValueError("Please, expand this function.")
+    
+if wlen_range is None:
+    wlen_range = get_wlen_range(material, submerged_index)
+epsilon_ext = (submerged_index)**2
 
 #%% SAVING CONFIGURATION
 
-series = f"{folder}{2*r:1.0f}"
-plot_folder = f"DataAnalysis/{folder}/{series}"
+series = f"All{vmt.recognize_material(submerged_index)}{material}Sphere{2*r:1.0f}"
 home = vs.get_home()
-if not os.path.isdir(os.path.join(home, plot_folder)): 
-    vs.new_dir(os.path.join(home, plot_folder))
+if not os.path.isdir(os.path.join(home, folder)): 
+    vs.new_dir(os.path.join(home, folder))
 plot_file = lambda n : os.path.join(
-    home, plot_folder, f"AllWaterField{2*r:1.0f}" + n)
+    home, folder, f"AllWaterField{2*r:1.0f}" + n)
+
+plot_title = trs.choose(f"{material} NP of {2*r:1.0f} nm diameter in {vmt.recognize_material(submerged_index).lower()}",
+                        f"NP de {material} de {2*r:1.0f} nm de diámetro en {vmt.recognize_material(submerged_index, english).lower()}")
 
 #%% INNER MEDIUM EPSILON
 
 wlen = np.linspace(*wlen_range, npoints)
 
-epsilon_func_meep_jc = vmt.epsilon_function(material="Au", 
+epsilon_func_meep_jc = vmt.epsilon_function(material=material, 
                                            paper="JC", 
                                            reference="Meep")
-epsilon_func_meep_r = vmt.epsilon_function(material="Au", 
+epsilon_func_meep_r = vmt.epsilon_function(material=material, 
                                           paper="R", 
                                           reference="Meep")
-epsilon_func_exp_jc = vmt.epsilon_function(material="Au", 
+epsilon_func_exp_jc = vmt.epsilon_function(material=material, 
                                           paper="JC", 
                                           reference="RIinfo")
-epsilon_func_exp_r = vmt.epsilon_function(material="Au", 
+epsilon_func_exp_r = vmt.epsilon_function(material=material, 
                                           paper="R", 
                                           reference="RIinfo")
 
@@ -67,38 +90,40 @@ epsilon = np.array([epsilon_meep_jc,
 wlen_long = np.array([*[wlen]*4])
 
 functions = [np.abs, np.real, np.imag]
-titles = ["Absolute value", "Real part", "Imaginary part"]
-ylabels = [r"|$\alpha$| [nm$^3$]", r"Re($\alpha$) [nm$^3$]", r"Im($\alpha$) [nm$^3$]"]
+titles = trs.choose([r"Absolute value |$\epsilon$|", r"Real part $\Re$e$(\epsilon$)", r"Imaginary part $\Im$m$(\epsilon$)$"],
+                    [r"Valor absoluto |$\epsilon$|", r"Parte real $\Re$e$(\epsilon)$", r"Parte imaginaria $\Im$m$(\epsilon$)"])
 labels = ["Meep: Johnson & Christy", "Meep: Rakic", 
           "RIinfo: Johnson & Christy", "RIinfo: Rakic"]
 
 nplots = len(functions)
 fig = plt.figure(figsize=(nplots*6.4, 6.4))
-axes = fig.subplots(ncols=nplots)
+axes = fig.subplots(ncols=nplots, sharex=True, sharey=True, 
+                    gridspec_kw={"wspace":0})
 
-max_value = []
-min_value = []
-for ax, f, t, y in zip(axes, functions, titles, ylabels):
-    for wl, eps, l in zip(wlen_long, epsilon, labels):
+colors = ["C2", "C1"]*2
+linestyles = [*["solid"]*2, *["dotted"]*2]
+transparencies = [*[.5]*2, *[1]*2]
+
+plt.suptitle(plot_title)
+for ax, f, t in zip(axes, functions, titles):
+    for wl, eps, l, col, lsy, tps in zip(wlen_long, epsilon, labels, 
+                                         colors, linestyles, transparencies):
         ax.set_title(t)
         print(l, eps[:2])
-        ax.plot(wl, f(eps), label=l)
-        ax.xaxis.set_label_text("Wavelength [nm]")
-        ax.yaxis.set_label_text(y)
+        ax.plot(wl, f(eps), label=l, linewidth=2, alpha=tps, color=col, linestyle=lsy)
+        ax.xaxis.set_label_text(trs.choose("Wavelength [nm]", "Longitud de onda [nm]"))
+        if ax == axes[0]:
+            ax.yaxis.set_label_text(trs.choose(r"Electric Permitivity $\epsilon$", 
+                                               r"Permitividad eléctrica $\epsilon$"))
         ax.legend()
         ax.set_xlim(*wlen_range)
-        max_value.append(max(f(eps)))
-        min_value.append(min(f(eps)))
-        
-for ax in axes: ax.set_ylim([min(min_value)-.1*(max(max_value)-min(min_value)), 
-                             max(max_value)+.1*(max(max_value)-min(min_value))])
     
 plt.savefig(plot_file("Epsilon.png"))
 
 #%% CLAUSIUS-MOSETTI: POLARIZABILITY
 
 alpha_cm_meep_r = vt.alpha_Clausius_Mosotti(epsilon_meep_r, r,
-                                           epsilon_ext=epsilon_ext)
+                                            epsilon_ext=epsilon_ext)
 alpha_cm_exp_jc = vt.alpha_Clausius_Mosotti(epsilon_exp_jc, r,
                                             epsilon_ext=epsilon_ext)
 
@@ -118,40 +143,41 @@ alpha_k = np.array([alpha_k_meep_r,
 #%% PLOT POLARIZABILITY
 
 functions = [np.abs, np.real, np.imag]
-titles = ["Absolute value", "Real part", "Imaginary part"]
-ylabels = [r"|$\alpha$| [nm$^3$]", r"Re($\alpha$) [nm$^3$]", r"Im($\alpha$) [nm$^3$]"]
+titles = trs.choose([r"Absolute value |$\alpha$|", r"Real part $\Re$e$(\alpha$)", r"Imaginary part $\Im$m$(\alpha$)$"],
+                    [r"Valor absoluto |$\alpha$|", r"Parte real $\Re$e$(\alpha)$", r"Parte imaginaria $\Im$m$(\alpha$)"])
 wlen_alpha = [*[wlen]*4]
 alpha = [*alpha_cm, *alpha_k]
-labels = ["Meep: Claussius-Mosotti", "Jhonson: Claussius-Mosotti", 
-          "Meep: Kuwata", "Jhonson: Kuwata"]
+labels = ["Meep: Claussius-Mosotti", "J&C: Claussius-Mosotti", 
+          "Meep: Kuwata", "J&C: Kuwata"]
 
 nplots = len(functions)
-fig = plt.figure(figsize=(nplots*6.4, 6.4))
-axes = fig.subplots(ncols=nplots)
+fig, axes = plt.subplots(ncols=nplots, sharex=True, sharey=True, 
+                         gridspec_kw={"wspace":0})
+fig.set_size_inches((nplots*6.4, 6.4))
 
-max_value = []
-min_value = []
-for ax, f, t, y in zip(axes, functions, titles, ylabels):
-    for wl, al, l in zip(wlen_alpha, alpha, labels):
+colors = ["C0", "C3"]*2
+linestyles = [*["solid"]*2, *["dotted"]*2]
+transparencies = [*[.5]*2, *[1]*2]
+
+plt.suptitle(plot_title)
+for ax, f, t in zip(axes, functions, titles):
+    for wl, al, l, col, lsy, trp in zip(wlen_alpha, alpha, labels,
+                                        colors, linestyles, transparencies):
         ax.set_title(t)
         print(l, eps[:10])
-        ax.plot(wl, f(al), label=l)
-        ax.xaxis.set_label_text("Wavelength [nm]")
-        ax.yaxis.set_label_text(y)
+        ax.plot(wl, f(al), label=l, color=col, linestyle=lsy, alpha=trp, linewidth=2)
+        ax.xaxis.set_label_text(trs.choose("Wavelength [nm]", "Longitud de onda [nm]"))
+        if ax == axes[0]:
+            ax.yaxis.set_label_text(trs.choose("Polarizability", "Polarizabilidad") + r" $\alpha$ [nm$^3$]")
         ax.legend()
         ax.set_xlim(*wlen_range)
-        max_value.append(max(f(al)))
-        min_value.append(min(f(al)))
-        
-for ax in axes: ax.set_ylim([min(min_value)-.1*(max(max_value)-min(min_value)), 
-                             max(max_value)+.1*(max(max_value)-min(min_value))])
     
 plt.savefig(plot_file("Polarizability.png"))
 
 #%% DIPOLAR APROXIMATION: ELECTRIC FIELD EZ IN Z
 
 rvec = np.zeros((npoints, 3))
-rvec[:,2] = np.linspace(-3*r, 3*r, npoints)
+rvec[:,2] = np.linspace(-4*r, 4*r, npoints)
 
 E_cm_meep_r_chosen = []
 E_k_meep_r_chosen = []
@@ -196,22 +222,32 @@ E_labels = ["Meep + Claussius-Mosotti",
 
 n = len(wlen_chosen)
 fig = plt.figure(figsize=(n*6.4, 6.4))
-axes = fig.subplots(ncols=n)
+axes = fig.subplots(ncols=n, sharex=True, sharey=True, 
+                    gridspec_kw={"wspace":0})
 
-E_max = []
-E_min = []
+colors = ["C0", "C3"]*2
+linestyles = [*["solid"]*2, *["dotted"]*2]
+transparencies = [*[.5]*2, *[1]*2]
+
+plt.suptitle(plot_title)
 for i in range(n):
     axes[i].set_title(f'$\lambda$={wlen_chosen[i]:1.0f} nm')
     for j in range(len(E_labels)):
-        axes[i].plot(rvec[:,2], np.real(E_chosen[j][i][:,2]))
-        E_max.append(np.max(np.real(E_chosen[j][i][:,2])))
-        E_min.append(np.min(np.real(E_chosen[j][i][:,2])))
-    axes[i].set_xlabel("Distance in z [u.a.]")
-    axes[i].set_ylabel("Electric field Ez [u.a.]")
-    axes[i].legend(E_labels)
-lims = [min(E_min), max(E_max)]
-lims = [lims[0] - 0.25*(lims[1]-lims[0]), lims[1] + 0.25*(lims[1]-lims[0])]
-for ax in axes: ax.set_ylim(*lims)
+        axes[i].axvline(-r, color="grey", linestyle="dotted", label="", alpha=0.5)
+        axes[i].axvline(r, color="grey", linestyle="dotted", label="", alpha=0.5)
+        axes[i].axvline(-2.5*r, color="grey", linestyle="dotted", label="", alpha=0.5)
+        axes[i].axvline(2.5*r, color="grey", linestyle="dotted", label="", alpha=0.5)
+        # axes[i].axhline(-2.5*r, color="grey", linestyle="dotted", label="", alpha=0.5)
+        # axes[i].axhline(2.5*r, color="grey", linestyle="dotted", label="", alpha=0.5)
+        axes[i].plot(rvec[:,2], np.abs(E_chosen[j][i][:,2]),
+                     color=colors[j], linestyle=linestyles[j],
+                     alpha=transparencies[j], linewidth=2, label=E_labels[i])
+    axes[i].set_xlabel(trs.choose(r"Position $Z$ [nm]", r"Posición $Z$ [nm]"))
+    if i==0:
+        axes[i].set_ylabel(trs.choose(r"Electric field $E_z$", "Campo eléctrico $E_z$"))
+    if (material=="Au" and i==0) or (material=="Ag" and i==n-1):
+        axes[i].legend()
+    # axes[i].set_xlim(np.min(rvec), np.max(rvec))
 
 plt.savefig(plot_file("FieldProfile.png"))
 
